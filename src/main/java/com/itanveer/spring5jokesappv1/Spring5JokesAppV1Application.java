26eb24e6d4f157b85fd34104869473c045b504c1
iTanveerAhmad <talktanveerkhan@outlook.com>
2021-06-29 11:15:54 +0200

    Create Spring Boot App. Closes #1

---
+++ src/main/java/com/itanveer/spring5jokesappv1/Spring5JokesAppV1Application.java
@@ -0,0 +1,13 @@
+package com.itanveer.spring5jokesappv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppV1Application.class, args);
+	}
+
+}
