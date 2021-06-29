716025671de0f7e67fdbc27d41e63bd55bda5fa8
iTanveerAhmad <talktanveerkhan@outlook.com>
2021-06-29 12:13:32 +0200

    Add Joke Controller. Closes #4

---
+++ src/main/java/com/itanveer/spring5jokesappv1/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package com.itanveer.spring5jokesappv1.controllers;
+
+import com.itanveer.spring5jokesappv1.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
